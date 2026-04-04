1231c3360b4b96f4b732a67ff1b6ba62ec39891b
vehicle-project <malleshwari.k06@gmail.com>
2026-04-04 19:26:10 +0100

    Initial commit of engagement-service source code

---
+++ engagement-service/src/test/java/com/example/engagementserviceapplication/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.engagementserviceapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
